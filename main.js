f074157c5d3e72a4aa004f54e6e5da542928f0d6
faheempro <115949245+faheempro@users.noreply.github.com>
2023-05-03 17:16:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+ let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+  
+    formValidation();
+  });
+  
+  
+  let formValidation = () => {
+    if (input.value === "") {
+      msg.innerHTML = "Post cannot be blank";
+      console.log("failure");
+    } else {
+      console.log("successs");
+      msg.innerHTML = "";
+    }
+    //if (input.value === "") {
+        // Other codes are here
+      //} else {
+        // Other codes are here
+        acceptData();
+      }
+
+};
+let data = {};
+
+let acceptData = () => {
+    data["text"] = input.value;
+  console.log(data);
+  createPost();
+
+};
+
+let createPost = () => {
+    posts.innerHTML += ``;
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+
+  };
+// delte post 
+
+let deletePost = (e) => {
+    //<i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    e.parentElement.parentElement.remove();
+};
+
+// eidit
+let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
+  };
